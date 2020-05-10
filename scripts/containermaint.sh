6ba5990c3d8aecb816cf30d3d3602339df2e0520
Jesse Borden <49888529+bordenit@users.noreply.github.com>
2020-05-09 21:09:47 -0400

    Update containermaint.sh

--- scripts/containermaint.sh
+++ scripts/containermaint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-$dockerContainerPath=$dockerHubUserName/$dockerHubContainerName
-echo $dockerHubPassword | docker login -u $dockerHubUsername --password-stdin
 CONTAINERID=$(docker ps -a -q)
 if [ ! -z "$CONTAINERID" ]
 then
@@ -9,5 +7,5 @@ docker rm $CONTAINERID 2>/dev/null || true
 else
 echo "No Containers to Remove"
 fi
-docker pull $dockerHubContainerPath
-docker run -v ~/x509stores/:/root/.dotnet/corefx/cryptography/x509stores/ --name mcmaster -d -p 80:80 -p 443:443 -e "ASPNETCORE_ENVIRONMENT=Production" -e ASPNETCORE_URLS="http://+;https://+" -e ASPNETCORE_HTTPS_PORT=443 $dockerHubContainerPath
+docker pull $dockerHubUsername/$dockerHubContainerName
+docker run -v ~/x509stores/:/root/.dotnet/corefx/cryptography/x509stores/ --name mcmaster -d -p 80:80 -p 443:443 -e "ASPNETCORE_ENVIRONMENT=Production" -e ASPNETCORE_URLS="http://+;https://+" -e ASPNETCORE_HTTPS_PORT=443 $dockerHubUsername/$dockerHubContainerName
