bcb60dcec478d3946d53486941f50079afc9a330
Jesse Borden <jesse.borden@bordenit.com>
2020-05-10 11:24:07 -0400

    Updated

--- scripts/containermaint.sh
+++ scripts/containermaint.sh
@@ -9,4 +9,4 @@ else
 echo "No Containers to Remove"
 fi
 docker pull $dockerHubUsername/$dockerHubContainerName
-docker run -v ~/x509stores/:/root/.dotnet/corefx/cryptography/x509stores/ --name mcmaster -d -p 80:80 -p 443:443 -e "ASPNETCORE_ENVIRONMENT=Production" -e ASPNETCORE_URLS="http://+;https://+" -e ASPNETCORE_HTTPS_PORT=443  $dockerHubUsername/$dockerHubContainerName -e "GITHUB_SHA=$GITHUB_SHA"
+docker run -v ~/x509stores/:/root/.dotnet/corefx/cryptography/x509stores/ --name mcmaster -d -p 80:80 -p 443:443 -e "ASPNETCORE_ENVIRONMENT=Production" -e ASPNETCORE_URLS="http://+;https://+" -e ASPNETCORE_HTTPS_PORT=443  $dockerHubUsername/$dockerHubContainerName -e "COMMITID=$COMMITID"
