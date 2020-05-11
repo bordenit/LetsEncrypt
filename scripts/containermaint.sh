d294b8737bee0994cdc1d7494c62dbb597160866
Jesse Borden <49888529+bordenit@users.noreply.github.com>
2020-05-11 16:02:20 -0400

    Master rebase (#20)
    Docker container testing proof of concept. Version .0005

---
+++ scripts/containermaint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo $dockerHubPassword | docker login -u $dockerHubUsername --password-stdin
+CONTAINERID=$(docker ps -a -q)
+docker stop $CONTAINERID > /dev/null
+docker rm $CONTAINERID 2>/dev/null || true
+docker pull $dockerHubUsername/$dockerHubContainerName
+docker run -v ~/x509stores/:/root/.dotnet/corefx/cryptography/x509stores/ --name $dockerHubContainerName -d -p 80:80 -p 443:443 -e "ASPNETCORE_ENVIRONMENT=Production" -e ASPNETCORE_URLS="http://+;https://+" -e ASPNETCORE_HTTPS_PORT=443 $dockerHubUsername/$dockerHubContainerName
